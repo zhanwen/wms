5485ed0954a8e7a186cf988f0a5f75760e5e6dd9
hanwen <1106002609@qq.com>
2016-03-22 21:16:04 +0800

    JsonViewResolver

---
+++ src/main/java/phoenics/wms/config/JsonViewResolver.java
@@ -0,0 +1,20 @@
+package phoenics.wms.config;
+
+import java.util.Locale;
+
+import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
+
+public class JsonViewResolver implements ViewResolver{
+
+    @Override
+    public View resolveViewName(String viewName, Locale locale) throws Exception {
+        MappingJackson2JsonView view = new MappingJackson2JsonView();
+        view.setExtractValueFromSingleKeyModel(true);
+        view.setPrettyPrint(true);
+        return view;
+    }
+
+
+}
